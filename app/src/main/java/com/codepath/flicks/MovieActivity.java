9d89b054a9d2bd5c3946d456d6e9c50919ec9a4d
Sukwon Lee <slee@hearsaycorp.com>
2018-09-22 13:30:09 -0700

    Initial commit. Display movies

---
+++ app/src/main/java/com/codepath/flicks/MovieActivity.java
@@ -0,0 +1,61 @@
+package com.codepath.flicks;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.codepath.flicks.adapters.MovieArrayAdapter;
+import com.codepath.flicks.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MovieActivity extends AppCompatActivity {
+
+    ArrayList<Movie> movies;
+    MovieArrayAdapter movieAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+        lvItems = findViewById(R.id.lvMovie);
+        movies = new ArrayList<>();
+        movieAdapter = new MovieArrayAdapter(this, movies);
+        lvItems.setAdapter(movieAdapter);
+
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray movieJSONResults = null;
+
+                try {
+                    movieJSONResults = response.getJSONArray("results");
+                    movies.addAll(Movie.fromJSONArray(movieJSONResults));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.d("DEBUG", movieJSONResults.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/codepath/flicks/adapters/MovieArrayAdapter.java
@@ -0,0 +1,48 @@
+package com.codepath.flicks.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.flicks.R;
+import com.codepath.flicks.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+
+public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+    public  MovieArrayAdapter(Context context, List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        ImageView ivImage = convertView.findViewById(R.id.ivMovie);
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = convertView.findViewById(R.id.tvOverView);
+
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+
+        return convertView;
+    }
+}
