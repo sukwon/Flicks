9bfb45d4cd83cf68b58e12f37af8e5e344ab495e
Sukwon Lee <slee@hearsaycorp.com>
2018-09-23 12:17:52 -0700

    Use OKHttp library for network service

--- app/src/main/java/com/codepath/flicks/MovieActivity.java
+++ app/src/main/java/com/codepath/flicks/MovieActivity.java
@@ -7,16 +7,19 @@ import android.widget.ListView;
 
 import com.codepath.flicks.adapters.MovieArrayAdapter;
 import com.codepath.flicks.models.Movie;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
-import cz.msebera.android.httpclient.Header;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -34,27 +37,41 @@ public class MovieActivity extends AppCompatActivity {
         movieAdapter = new MovieArrayAdapter(this, movies);
         lvItems.setAdapter(movieAdapter);
 
+        fetchMovies();
+    }
+
+    private void fetchMovies() {
         String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(url, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                JSONArray movieJSONResults = null;
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
 
-                try {
-                    movieJSONResults = response.getJSONArray("results");
-                    movies.addAll(Movie.fromJSONArray(movieJSONResults));
-                    movieAdapter.notifyDataSetChanged();
-                    Log.d("DEBUG", movieJSONResults.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
             }
 
             @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                super.onFailure(statusCode, headers, responseString, throwable);
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String responseData = response.body().string();
+                    JSONObject json = new JSONObject(responseData);
+                    final JSONArray movieJSONResults = json.getJSONArray("results");
+                    Log.d("DEBUG", movieJSONResults.toString());
+
+                    MovieActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            movies.addAll(Movie.fromJSONArray(movieJSONResults));
+                            movieAdapter.notifyDataSetChanged();
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
